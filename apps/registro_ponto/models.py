1e56eca5dfbc2c004f44de384e7ac7970f5f2e79
jhonnymalta <jhonatamalta@hotmail.com>
2023-08-16 22:09:12 -0300

    create registe home page

--- apps/registro_ponto/models.py
+++ apps/registro_ponto/models.py
@@ -4,5 +4,11 @@ from apps.funcionarios.models import Funcionario
 # Create your models here.
 class Registro_Ponto(models.Model):
     dia = models.DateTimeField()
+    hora_ini = models.DateTimeField()
+    hora_entrada_lunch = models.DateTimeField()
+    hora_retorno_lunch = models.DateTimeField()
+    hora_fim = models.DateTimeField()
+    hora_ini_extra = models.DateTimeField()
+    hora_fim_extra = models.DateTimeField()
     funcionario = models.ForeignKey(Funcionario,on_delete=models.PROTECT)
 
